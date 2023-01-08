8e02e7b125fa8d6c46cd873929d41569a6ac736c
Thai Core <linhhongio.nguyen28@gmail.com>
2023-01-08 18:56:34 +0900

    Update code 2

--- Hello/src/com/vti/Hello.java
+++ Hello/src/com/vti/Hello.java
@@ -4,9 +4,13 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		
+
 		System.out.println("Hello");
 		System.out.println("Hello");
 		System.out.println("Hello");
+
+		System.out.println("Hello1");
+		System.out.println("Hello2");
+
 	}
 }
